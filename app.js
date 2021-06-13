91e7add4d1aac642609e617f86b6fdff1c9905b8
ahmedsujon <ahmedsujon050@gmail.com>
2021-06-13 08:10:43 +0600

    Method and Events

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var app = Vue.createApp ({
     data() {
         return{
+            
+            count: 0,
+
             msg: "Welcome To VueJs adnance 3",
             projectInfo:{
                 name: "Vue Js 3 Advance",
                 github: "https://github.com/ahmedsujon"
             }
         };
+    },
+
+    methods: {
+        increase(){
+            this.count = this.count +1;
+        },
+
+        decrease(){
+            this.count = this.count -1;
+        },
+
+        getCurrentTime() {
+            let ct = new Date();
+            return new Date();
+        }
     }
+    
 });
 
 app.mount('#app');
